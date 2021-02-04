216716b574f2a73b49ca44373c1be046e1646320
ElAmirr <othmaniamir41@gmail.com>
2021-02-04 15:48:54 +0100

    push it all

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+
+#include <stdio.h>
+/**
+*main - maine function
+*Return: 0
+*/
+int main(void)
+{
+	unsigned int i;
+
+	for (i = 0; i <= 9; ++i)
+	{
+		printf("%i", i);
+	}
+	putchar('\n');
+	return (0);
+}
