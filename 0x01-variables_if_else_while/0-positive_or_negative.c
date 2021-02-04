40f47feca0aef7dde19eade068ce9d2ef0aa30b7
ElAmirr <othmaniamir41@gmail.com>
2021-02-04 13:51:47 +0100

    0-positive_or_negative push file

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -8,17 +8,20 @@
 */
 int main(void)
 {
-srand(time(NULL));// Initialization, should only be called once.
-int r = rand(); //returns a pseudo-random integer between 0 and RAND_MAX
-if( r>0 ) {
+srand(time(NULL));
+int r = rand();
+if (r > 0)
+{
 	/* if condition is true then print the following */
-	printf("%i is positive \n",r);
-} else if ( r<0 ) {
+	printf("%i is positive \n", r);
+} else if (r < 0)
+{
 	/* if condition is true then print the following */
-	printf("%i is negative \n",r);
-} else {
+	printf("%i is negative \n", r);
+} else
+{
 	/* if condition is true then print the following */
-	printf("%i is zero \n",r);
+	printf("%i is zero \n", r);
 }
 return (0);
 }
