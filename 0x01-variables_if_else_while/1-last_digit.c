dafd3a48da63180f349a164cddb32a6c0e21d835
ElAmirr <othmaniamir41@gmail.com>
2021-02-04 15:41:05 +0100

    bettu style

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,32 +1,33 @@
+#include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-#include <stdlib.h>
+
 /**
-* main - Entry point
+*main - maine function
 *
-* Return: Always 0 (Success)
+*Return: 0
 */
 int main(void)
 {
-	int n;
-	int is;
+int n;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	if (n % 10 > 5)
+	printf("Last digit of %d is %d", n, n % 10);
+	if (n % 10 == 0)
 	{
-		/** if condition is true then print the following */
-		printf("Last digit of %i is %i and is greater than 5\n", n, is);
-	}
-	else if (n % 10 < 6 && n % 10 != 0)
-	{
-		/** if condition is true then print the following */
-		printf("Last digit of %i is %i and is less than 6 and not 0\n", n, is);
+		printf(" and is 0\n");
 	}
 	else
 	{
-		/** if condition is true then print the following */
-		printf("Last digit of %i is %i and is 0 \n", n, is);
-	}
+		if (n % 10 > 5)
+		{
+			printf(" and is greater than 5\n");
+		}
+		if (n % 10 < 6)
+		{
+			printf(" and is less than 6 and not 0\n");
+		}
+}
 	return (0);
 }
