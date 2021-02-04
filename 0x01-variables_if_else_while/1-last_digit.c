c0a3d782788a150616f531d9668413da62a841df
ElAmirr <othmaniamir41@gmail.com>
2021-02-04 15:23:30 +0100

    bettu style

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -17,17 +17,17 @@ int main(void)
 	if (is > 5)
 	{
 		/** if condition is true then print the following */
-		printf("Last digit of %i is %i and is greater than 5\n", is, n);
+		printf("Last digit of %i is %i and is greater than 5\n", n, is);
 	}
 	else if (is < 6 && is != 0)
 	{
 		/** if condition is true then print the following */
-		printf("Last digit of %i is %i and is less than 6 and not 0\n", is, n);
+		printf("Last digit of %i is %i and is less than 6 and not 0\n", n,is);
 	}
 	else
 	{
 		/** if condition is true then print the following */
-		printf("Last digit of %i is %i and is 0 \n", is, n);
+		printf("Last digit of %i is %i and is 0 \n", n, is);
 	}
 	return (0);
 }
