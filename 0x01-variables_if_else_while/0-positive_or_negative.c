d398143475259478a0b26a57461f4dd2b0d36b48
ElAmirr <othmaniamir41@gmail.com>
2021-02-04 14:18:09 +0100

    push it all

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -13,15 +13,15 @@ srand(time(0));
 n = rand() - RAND_MAX / 2;
 if (n > 0)
 {
-	/* if condition is true then print the following */
+  /** if condition is true then print the following */
 	printf("%i is positive\n", n);
 } else if (n < 0)
 {
-	/* if condition is true then print the following */
+  /** if condition is true then print the following */
 	printf("%i is negative\n", n);
 } else
 {
-	/* if condition is true then print the following */
+  /** if condition is true then print the following */
 	printf("%i is zero\n", n);
 }
 return (0);
