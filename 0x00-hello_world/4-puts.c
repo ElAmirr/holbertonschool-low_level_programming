acedc1ee8d5602302faef97a4db2c956a10d3496
ElAmirr <othmaniamir41@gmail.com>
2021-02-04 09:35:59 +0100

    update puts function

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -7,9 +7,9 @@
  */
 int main(void)
 {
-char str1[52];
-strcpy(str1, "\"Programming is like building a multilingual puzzle");
-puts(str1);
+char str[52];
+strcpy(str, "\"Programming is like building a multilingual puzzle");
+puts(str);
 
 return (0);
 }
