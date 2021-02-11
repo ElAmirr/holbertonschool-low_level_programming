20edfb1ecf739f34bda6688d981a4ad15c7bff28
ElAmirr <othmaniamir41@gmail.com>
2021-02-11 13:27:11 +0100

    push mull function

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+
+#include "holberton.h"
+/**
+*mul - main function that multiplies a couple of integers.
+*@a: integer.
+*@b: integer.
+*Return: a*b.
+*/
+int mul(int a, int b)
+{
+return (a*b);
+}
