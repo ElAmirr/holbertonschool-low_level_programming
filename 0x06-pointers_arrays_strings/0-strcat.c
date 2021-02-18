a5045b7b502410cec5efaa79eeffbf1a0ec961ff
ElAmirr <othmaniamir41@gmail.com>
2021-02-18 14:40:55 +0100

    last update for strcat function

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,19 +1,23 @@
+#include "holberton.h"
+/**
+ * _strcat - concatenates two strings
+ * @dest: concatination string
+ * @src: append string
+ * Return: On success dest.
+ */
 char *_strcat(char *dest, char *src)
 {
-    //move till the end of s1
-    int i = 0;
-    while(dest[i] != '\0')
-    {
-        i++;
-    }
-    //coppuy s to s1
-    int j = 0;
-
-    while(src[j] != '\0')
-    {
-        dest[i] = src[j];
-        i++;
-        j++;
-    }
-    dest[i] = '\0';
-return dest; 
+int i = 0;
+int j = 0;
+while (dest[i] != '\0')
+{
+i++;
+}
+while (src[j] != '\0')
+{
+dest[i] = src[j];
+i++;
+j++;
+}
+dest[i] = '\0';
+return (dest);
