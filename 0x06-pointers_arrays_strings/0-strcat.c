77ec9d949a161c4102955a3aea5b39290cd82c72
ElAmirr <othmaniamir41@gmail.com>
2021-02-18 14:01:44 +0100

    push strcat function

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+char *_strcat(char *dest, char *src)
+{
+    //move till the end of s1
+    int i = 0;
+    while(dest[i] != '\0')
+    {
+        i++;
+    }
+    //coppuy s to s1
+    int j = 0;
+
+    while(src[j] != '\0')
+    {
+        dest[i] = src[j];
+        i++;
+        j++;
+    }
+    dest[i] = '\0';
+    return (dest);    
+}
