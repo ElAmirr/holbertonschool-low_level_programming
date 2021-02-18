2d74a27dae36fd1cf21eeb7b6835c684e554c4c1
ElAmirr <othmaniamir41@gmail.com>
2021-02-18 21:22:43 +0100

    small changement in strncat function

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -9,18 +9,19 @@
  */
 char *_strncat(char *dest, char *src, int n)
 {
-int j, i = 0;
+int i = 0;
+int j = 0;
 while (dest[i] != '\0')
 {
 i++;
 }
-for (j = 0; j <= n; j++)
+while (src[j] != '\0' && j<n)
 {
-if (src[j] != '\0')
-{
+
 dest[i] = src[j];
 i++;
-}
+j++;
+
 }
 dest[i] = '\0';
 return (dest);
