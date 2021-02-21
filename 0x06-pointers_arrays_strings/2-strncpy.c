86fd72419a43b983bd7779a69653c880c4a09b8b
ElAmirr <othmaniamir41@gmail.com>
2021-02-21 18:04:59 +0100

    fix strcpy code

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,27 +1,23 @@
 #include "holberton.h"
 /**
- * _strncpy - concatenates two strings
- * @dest: coppy string
- * @src: append string
- * @n: nuber of bytes
- * Return: _strncpy
+ * _strncpy - a function that copirs a string
+ * @dest: pointer to char
+ * @src: pointer to a char
+ * @n: nuber of int
+ * Return: dest
  */
 char *_strncpy(char *dest, char *src, int n)
 {
 int i = 0;
-int j = 0;
-while (dest[i] != '\0')
+while (src[i] != '\0' && i < n)
 {
+dest[i] = src[i];
 i++;
 }
-while (src[j] != '\0' && j < n)
+while (i < n)
 {
-
-dest[i] = src[j];
-i++;
-j++;
-
-}
 dest[i] = '\0';
+i++;
+}
 return (dest);
 }
