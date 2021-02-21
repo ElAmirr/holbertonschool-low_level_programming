8163e61420b1d073d148c1178d1f0890a019d364
ElAmirr <othmaniamir41@gmail.com>
2021-02-21 19:16:03 +0100

    strcmp optimized code

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -5,23 +5,12 @@
 * @s2: second string.
 * Return: value.
 */
-
 int _strcmp(char *s1, char *s2)
 {
-
-int j, i;
-
-i = 0;
-j = 0;
-	while (s1[i] != '\0')
-	{
-	i++;
-	}
-
-		while (s1[j] == s2[j] && j < i)
-		{
-		j++;
-		}
-
-return (s1[j] - s2[j]);
+int i = 0;
+while (s1[i] == s2[i] && s1[i] != '\0')
+{
+i++;
+}
+return (s1[i] - s2[i]);
 }
