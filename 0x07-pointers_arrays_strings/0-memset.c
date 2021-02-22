a8c5544ae67f31d86c093b6a988090d42c4a2575
ElAmirr <othmaniamir41@gmail.com>
2021-02-22 13:28:39 +0100

    fix the return value in the comment section

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -4,7 +4,7 @@
  * @s: starting address of memory to be filed.
  * @b: value to be filed.
  * @n: nuber of bytes to be filled starting fron ptr to be filled.
- * Return: _memset
+ * Return: @s filled from b.
  */
 char *_memset(char *s, char b, unsigned int n)
 {
