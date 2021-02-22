35dfd6a5936f09804e479bd602685e8a6b88509c
ElAmirr <othmaniamir41@gmail.com>
2021-02-22 13:15:14 +0100

    an other code fix

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,9 +1,9 @@
 #include "holberton.h"
 /**
- * _memset - concatenates two strings
- * @s: starting address of memory to be filed
- * @b: value to be filed
- * @n: nuber of bytes to be filled starting fron ptr to be filled
+ * _memset - function that fills memory with a constant byte.
+ * @s: starting address of memory to be filed.
+ * @b: value to be filed.
+ * @n: nuber of bytes to be filled starting fron ptr to be filled.
  * Return: _memset
  */
 char *_memset(char *s, char b, unsigned int n)
@@ -11,6 +11,5 @@ char *_memset(char *s, char b, unsigned int n)
 unsigned int i;
 for (i = 0; i < n; i++)
 s[i] = b;
-s[i] = '\0';
 return (s);
 }
