699febbed4b8ab3c5d5d679ad89ecc270b496084
ElAmirr <othmaniamir41@gmail.com>
2021-02-22 12:51:40 +0100

    push the _memset function

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+/**
+ * _strncat - concatenates two strings
+ * @s: starting address of memory to be filed
+ * @b: value to be filed
+ * @n: nuber of bytes to be filled starting fron ptr to be filled
+ * Return: _memset
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+unsigned int i;
+for (i = 0; i <= n; i++)
+s[i] = b;
+return (s);
+}
