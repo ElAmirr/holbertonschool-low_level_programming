72843ad7ac12a4f9467635370a11242b8ce5ef8a
ElAmirr <othmaniamir41@gmail.com>
2021-02-22 14:19:16 +0100

    push _memcpy function

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * _memcpy - function that copies memory area.
+ * @dest: This is pointer to the destination array where the content is to be copied.
+ * @src: This is pointer to the source of data to be copied.
+ * @n: This is the number of bytes to be copied.
+ * Return: This function returns a pointer to destination, which is @dest
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+unsigned int i = 0;
+unsigned int j = 0;
+while (dest[i] != '\0')
+i++;
+while (src[j] != '\0' && j <= n)
+{
+dest[i] = src[j];
+i++;
+j++;
+}
+return (dest);
+}
