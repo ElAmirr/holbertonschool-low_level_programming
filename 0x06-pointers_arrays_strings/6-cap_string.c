2c5b9869f94926f618d3b97d5c11d5046f59d091
ElAmirr <othmaniamir41@gmail.com>
2021-03-31 12:48:48 +0100

    ask a new correction

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,20 +1,32 @@
 #include "holberton.h"
 /**
-* string_toupper - function that turns a string to lowercase to uppercase.
+* cap_string - Function that capitalizes all words of a string.
 * @str: string.
-* Return: A string in uppercase
+* Return: a capitalized string.
 */
 
-char *string_toupper(char *str)
+char *cap_string(char *str)
 {
 int i;
 
-	for (i = 0; str[i] != '\0'; i++)
+i = 1;
+	if (str[0] >= 'a' && str[0] <= 'z')
 	{
-		if (str[i] >= 'a' && str[i] <= 'z')
-		{
-		str[i] = str[i] - 'a' + 'A';
-		}
+	str[0] = str[0] - 'a' + 'A';
 	}
+		for (i = 0; str[i] != '\0'; i++)
+		{
+			if (str[i - 1] == ' ' || str[i - 1] ==  ',' ||
+		str[i - 1] ==  '.' || str[i - 1] ==  ';' || str[i - 1] ==  '{' ||
+		str[i - 1] ==  '!' || str[i - 1] ==  '(' || str[i - 1] ==  '}' ||
+		str[i - 1] ==  '?' || str[i - 1] ==  ')' ||
+		str[i - 1] == 9 || str[i - 1] == 10 ||  str[i - 1] == 0 || str[i - 1] == 32)
+		{
+			if (str[i] >= 'a' && str[i] <= 'z')
+			{
+			str[i] = str[i] - 'a' + 'A';
+			}
+	}
+		}
 return (str);
 }
