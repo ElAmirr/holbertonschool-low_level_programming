f70a00939bd20b2b29f873672efa560a2b98ee82
ElAmirr <othmaniamir41@gmail.com>
2021-03-31 12:51:51 +0100

    ask a new correction

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,23 +1,26 @@
 #include "holberton.h"
 /**
- * _strncpy - a function that copirs a string
- * @dest: pointer to char
- * @src: pointer to a char
- * @n: nuber of int
- * Return: dest
- */
+* *_strncpy - main function that copies a string.
+* @dest: destination.
+* @src: source.
+* @n: integer.
+* Return: dest.
+*/
+
 char *_strncpy(char *dest, char *src, int n)
 {
-int i = 0;
-while (src[i] != '\0' && i < n)
-{
-dest[i] = src[i];
-i++;
-}
-while (i < n)
-{
-dest[i] = '\0';
-i++;
-}
+int i;
+
+	for (i = 0; i < n && src[i] != '\0'; ++i)
+	{
+	dest[i] = src[i];
+	}
+
+		while (i < n)
+		{
+		dest[i] = '\0';
+		i++;
+		}
+
 return (dest);
 }
